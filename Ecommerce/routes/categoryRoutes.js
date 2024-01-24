f93854fe358f00f9fb06ca8f34fff5cec41ede50
hossenali2200 <hosenali2200@gmail.com>
2024-01-24 07:10:32 +0600

    mern-ecommerce
    A simple ecommerce website

---
+++ Ecommerce/routes/categoryRoutes.js
@@ -0,0 +1,36 @@
+ import express from "express"
+import { isAdmin, requireSignIn } from "../middlewares/authmiddleware.js";
+import { categoryControlller, createCategoryController, deleteCategoryCOntroller, singleCategoryController, updateCategoryController } from "../controllers/categoryController.js";
+
+
+
+ const router = express.Router()
+
+
+ //routes
+
+ //create category
+
+ router.post(
+    "/create-category",
+    requireSignIn,
+    isAdmin,
+    createCategoryController
+  );
+
+  //update category
+  router.put('/update-category/:id', requireSignIn, isAdmin, updateCategoryController)
+
+//get All category
+router.get('/get-category', categoryControlller)
+
+//single category
+router.get('/single-category/:slug', singleCategoryController)
+
+
+//delete category
+
+router.delete('/delete-category/:id', requireSignIn, isAdmin, deleteCategoryCOntroller)
+
+
+  export default router
